b52eea5b689f78a6af37efd7b9fd22177a027508
Velikoborets <gtahzby1@gmail.com>
2024-11-14 13:57:18 +0300

    feat: create configured DataBaseLoggerHandler

---
+++ app/Logging/DatabaseLoggerHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Logging;
+
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+use App\Models\Log;
+
+class DatabaseLoggerHandler
+{
+    /**
+     * Writes the log record to the database
+     *
+     * @param array $record The log record to write
+     *
+     * @return void
+     */
+    protected function write(array $record): void
+    {
+        Log::create(['message' => $record['message']]);
+    }
+}
